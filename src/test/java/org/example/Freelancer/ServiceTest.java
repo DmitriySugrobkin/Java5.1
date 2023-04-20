d60106ce03e4e56600ff38c031317c1f9818973f
Sugrobkin <dsugrobkin148@gmail.com>
2023-04-20 22:17:46 +0300

    First commit

---
+++ src/test/java/org/example/Freelancer/ServiceTest.java
@@ -0,0 +1,29 @@
+package org.example.Freelancer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+    @Test
+    public void calculateTest1() {
+        Service service = new Service();
+
+       int actual = service.calculate(10_000, 3000, 20_000 );
+       int expected = 3;
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void calculateTest2() {
+        Service service = new Service();
+
+        int actual = service.calculate(100_000, 60_000, 150_000 );
+        int expected = 2;
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+}
