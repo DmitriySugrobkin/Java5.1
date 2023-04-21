bfe110eac200a31f2f6c16661e9fd7997420985e
Sugrobkin <dsugrobkin148@gmail.com>
2023-04-21 16:53:15 +0300

    Reformate code

--- src/test/java/org/example/Freelancer/ServiceTest.java
+++ src/test/java/org/example/Freelancer/ServiceTest.java
@@ -10,20 +10,20 @@ class ServiceTest {
     public void calculateTest1() {
         Service service = new Service();
 
-       int actual = service.calculate(10_000, 3000, 20_000 );
-       int expected = 3;
+        int actual = service.calculate(10_000, 3000, 20_000);
+        int expected = 3;
 
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void calculateTest2() {
         Service service = new Service();
 
-        int actual = service.calculate(100_000, 60_000, 150_000 );
+        int actual = service.calculate(100_000, 60_000, 150_000);
         int expected = 2;
 
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 
 }
